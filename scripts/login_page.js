cc959eade1ff7cd0e1653cfaee47d2dbd5bdafa4
SNOUSSI <116595994+Noussii@users.noreply.github.com>
2023-05-14 23:36:08 +0100

    General update
    Responsive login-page
    Alert message fixed

--- scripts/login_page.js
+++ scripts/login_page.js
@@ -1,10 +1,19 @@
-function showErr(){
-    document.querySelector('.alert-message').style.display = 'flex';
-}
-
-
-let searchQuery = new URLSearchParams(window.location.search);
-if(searchQuery.get('err')){
+function showErr() {
+    var alertEl = document.getElementById('alert-message');
+    alertEl.classList.add('show');
+    alertEl.style.display = 'block';
+  }
+  
+  let searchQuery = new URLSearchParams(window.location.search);
+  if (searchQuery.get('err')) {
     showErr();
+  }
+
+function Close(){
+    var alertDel=document.querySelector('.btn-close')
+    var alertSection=document.querySelector('#alert-message')
+    if(alertDel){
+        alertSection.remove()
+    }
 }
 
