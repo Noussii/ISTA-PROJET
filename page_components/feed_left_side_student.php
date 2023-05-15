39faa2a52d8095c320f277ac675f42d8ed01c71b
iliass1234 <expert.ilyasse@outlook.com>
2023-05-15 20:46:12 +0100

    changes

--- page_components/feed_left_side_student.php
+++ page_components/feed_left_side_student.php
@@ -97,14 +97,14 @@
         <a href='../page/feed.php' class="left-side-button">
             <img src="../media/icons/home_icon_feed.png"> <span>Feed&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
         </a>
+        <a href='../page/notes.php' class="left-side-button">
+            <img src="../media/icons/notes_icon_feed.png"> <span>Notes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        </a>
+        <a href='../page/class.php' class="left-side-button">
+        <img src="../media/icons/class_icon_feed.png"> <span>Class&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        </a>
         <a href='../index.php' class="left-side-button">
         <img src="../media/icons/homework_icon_feed.png"> <span>Homework&nbsp;&nbsp;</span>
-        </a>
-        <a href='../page/notes.php' class="left-side-button">
-        <img src="../media/icons/notes_icon_feed.png"> <span>Notes&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-        </a>
-        <a href='../index.php' class="left-side-button">
-        <img src="../media/icons/class_icon_feed.png"> <span>Class&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
         </a>
         <a href='../index.php' class="left-side-button">
         <img src="../media/icons/newsLetter_icon_feed.png"> <span>Newsletter</span>
