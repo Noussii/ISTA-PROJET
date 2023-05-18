c35942c18785256db8d391b986414862e12ef8f2
SNOUSSI <116595994+Noussii@users.noreply.github.com>
2023-05-18 23:09:50 +0100

    General update
    Adding footer

--- classes/Dbc.php
+++ classes/Dbc.php
@@ -12,7 +12,7 @@ class Dbc {
 
         $this->servername = 'localhost';
         $this->username = 'root';
-        $this->password = 'wtf123';
+        $this->password = '';
         $this->dbname = 'ista_website_db';
         $this->charset = 'utf8mb4';
         
--- index.php
+++ index.php
@@ -123,6 +123,46 @@
         </section>
     </main>
 
+
+    <footer class="text-center text-lg-start text-white">
+    <div class="container p-4 pb-0">
+        <section >
+        <div class="row">
+                <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">E-ISTALAZARET</h6>
+                    <p>institution since 1945 this Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sed distinctio harum velit iure quod.</p>
+                </div>
+            <!-- Do not delete this line; shown only in sm / used to separate divs with hr -->
+            <hr class="w-100 d-md-none" />
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                    <p><a href="./page/about.php" class="text-white">About us</a> </p>
+                    <p><a href="./page/article.php" class="text-white">Articles</a></p>          
+                    <p><a href="./page/updates.php" class="text-white">Updates</a></p>           
+                    <p><a href="./page/newsLetter.php" class="text-white">Newsletter</a></p>
+                </div>
+            <hr class="w-100 d-md-none" />
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                    <p><i class="fas fa-home mr-3"></i> ISTA LAZARET, OUJDA</p>
+                    <p><i class="fas fa-envelope mr-3"></i> istalazaret@ofppt-edu.ma</p>
+                    <p><i class="fas fa-phone mr-3"></i> + 212 123 456 708</p>
+                </div>
+        </div>
+        </section>
+        <hr class="my-3">
+
+            <section class="p-3 pt-0">
+                <div class="row d-flex align-items-center">
+                <div class="col-md-7 col-lg-8 text-center text-md-start">
+                    <div class="p-3">© 2023 Copyright:<a class="text-white" href="index.php">e-istalazaret.com</a></div>    
+                </div>
+                </div>
+            </section>
+    </div>
+    </footer>
+
     <script src="/scripts/index.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.1/js/bootstrap.bundle.min.js"></script>
 
