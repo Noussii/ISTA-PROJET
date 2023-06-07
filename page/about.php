c1234afa7a3c9cced6e132daaff62baedbe93caa
SNOUSSI <116595994+Noussii@users.noreply.github.com>
2023-06-08 00:28:40 +0100

    General update

--- page/about.php
+++ page/about.php
@@ -32,25 +32,23 @@
                 </div>
                 <div class=" col-md-6 text-center">
                     <h1 class=" fw-bold">À propos de nous</h1>
-                    <p class="p-about">Ista lazaret Oujda is a vocational training institute located in Oujda, a city in northeastern Morocco.
-                         The institute offers a range of technical and professional training programs designed to prepare students for careers in various fields.
+                    <p class="p-about">ISTA Lazaret Oujda est un institut de formation professionnelle situé à Oujda, au Maroc. Il propose une variété de programmes de formation dans divers domaines  tels que l'informatique, la comptabilité, , gestion des Entreprises et bien d'autres. 
+                        L'institut vise à fournir aux stagiaires les compétences pratiques nécessaires pour réussir sur le marché du travail et répondre aux besoins de l'industrie.
+                         ISTA Lazaret Oujda se concentre sur l'apprentissage pratique en offrant des installations modernes et en mettant l'accent sur les aspects pratiques des programmes de formation.
                         </p>
-                    <p class="p-about">Ista lazaret Oujda focuses on providing practical and industry-relevant education to equip students with the skills needed for the job market. 
-                        The institute offers programs in areas such as information technology, electronics, electrical engineering, mechanics, construction, and more.</p>
+                    <!-- <p class="p-about">Ista lazaret Oujda focuses on providing practical and industry-relevant education to equip students with the skills needed for the job market. 
+                        The institute offers programs in areas such as information technology, electronics, electrical engineering, mechanics, construction, and more.</p> -->
                         <div class="row">
                             <div class="col-md-6 col-sm-12">
                                 <h3 class=" fw-bold">Mission</h3>
-                                <p class="p-about">Ista lazaret Oujda is a vocational training institute located in Oujda, a city in northeastern Morocco.
-                         The institute offers a range of technical and professional training programs designed to prepare students for careers in various fields.
-                         Ista lazaret Oujda is a vocational training institute located in Oujda, a city in northeastern Morocco.
-                         The institute offers a range of technical and professional training programs designed to prepare students for careers in various fields.</p>
+                                <p class="p-about">Notre mission à ISTA Lazaret Oujda est de former des professionnels hautement qualifiés en leur fournissant un environnement d'apprentissage dynamique et axé sur la pratique. 
+                                    Nous visons à préparer nos stagiaires à exceller dans leur domaine respectif en répondant aux besoins actuels de l'industrie.
+                                </p>
                             </div>
                             <div class="col-md-6 col-sm-12 ">
                                 <h3 class=" fw-bold">Vision</h3>
-                                <p class="p-about">Ista lazaret Oujda is a vocational training institute located in Oujda, a city in northeastern Morocco.
-                         The institute offers a range of technical and professional training programs designed to prepare students for careers in various fields.
-                         Ista lazaret Oujda is a vocational training institute located in Oujda, a city in northeastern Morocco.
-                         The institute offers a range of technical and professional training programs designed to prepare students for careers in various fields.</p>
+                                <p class="p-about">Notre vision à ISTA Lazaret Oujda est d'être un leader reconnu dans la formation professionnelle au Maroc, en offrant une éducation technique de qualité et en répondant aux besoins du marché du travail. 
+                                    Nous cherchons à maintenir un environnement d'apprentissage innovant et stimulant, en mettant à jour nos programmes pour refléter les dernières avancées technologiques, afin de former des professionnels hautement qualifiés et compétents.</p>
                             </div>
                         </div>
                 </div>
